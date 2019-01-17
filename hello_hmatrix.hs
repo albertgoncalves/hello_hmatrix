4de2a0ee10340b701a73718b9dd91641144dcc7f
albertgoncalves <goncalves.albert@gmail.com>
2019-01-16 22:22:31 -0500

    Worked with it a bit more. This is good stuff.

--- hello_hmatrix.hs
+++ hello_hmatrix.hs
@@ -10,19 +10,22 @@ v :: Vector R
 v = vector [10, 20, 30]
 
 a :: Matrix R
-a = (3 >< 5) [1 ..] -- don't try to print this!
+a = (3 >< 5) [1 ..]
 
 b :: Matrix R
 b = (5 >< 2) [1, 3, 0, 2, -1, 5, 7, 7, 6, 0]
 
-batchPrint :: (Show a) => [a] -> IO ()
-batchPrint = mapM_ print
+c :: Matrix R
+c = uniformSample 1 5 $ replicate 3 (0.0, 1.0) -- 4x5 random uniform
+
+dispList :: Int -> [Matrix Double] -> IO ()
+dispList = mapM_ . disp
 
 main :: IO ()
 main = do
     print $ m #> v -- matrix-vector product
     print $ v <.> v -- dot product of 2 vectors
-    batchPrint
-        [ b -- show matrix
-        , a <> b -- matrix-matrix product
-        ]
+    dispList
+        3 -- decimal precision to print
+        [a, b, a <> b, c, c <> a]
+        --        \----------\---- matrix-matrix product
